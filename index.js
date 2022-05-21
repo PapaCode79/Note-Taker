0016f628dde7a426271855fe544a941d0a6f0b88
Badara Dia <pushsoccer79@gmail.com>
2022-05-20 22:10:58 -0400

    express setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const fs = require("fs")
+const db = require("./db/db.json")
+
+const app = express();
+const PORT = process.env.PORT ||3000
+app.use(express.json())
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+
+app.listen(PORT, function () {
+    console.log("appislistening")
+})
